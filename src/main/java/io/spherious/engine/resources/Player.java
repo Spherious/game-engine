8a06d2bff6f834528bd0cd47f10c6e4150236a42
Yoursole <matinzzz@icloud.com>
2022-03-08 20:49:09 -0800

    *commented* hehe

--- src/main/java/io/spherious/engine/resources/Player.java
+++ src/main/java/io/spherious/engine/resources/Player.java
@@ -14,6 +14,15 @@ public class Player {
     private Vector2D movement;
     private ArrayList<Point> savedPoints = null;
 
+    /**
+     *
+     * @param x starting movement x
+     * @param y starting movement y
+     * @param size size (radius)
+     * @param locx starting location x
+     * @param locy starting location y
+     * @param defaultSize default size for respawn
+     */
     public Player(float x, float y, int size, int locx, int locy, int defaultSize) {
         this.movement = new Vector2D(x, y);
         this.size = size;
@@ -64,6 +73,15 @@ public class Player {
     }
     //save the points in a circle of the player size around 0,0 to optimize
 
+    /**
+     * On the first run, it generates a circle of points around 0,0 with a radius of the player's size
+     * Next it adds the location of the player to each of these points to get all points inside the player
+     * at the current location
+     *
+     * On all subsequent runs, it uses the saved points around 0,0 and just shifts them - which increases speed
+     *
+     * @return all integer points inside the border or the player
+     */
     public ArrayList<Point> getPointsInside() {
         ArrayList<Point> points = new ArrayList<>();
         if (this.savedPoints == null) {
